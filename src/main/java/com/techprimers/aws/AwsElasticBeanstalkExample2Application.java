587baa260087bc7de818e683e45f598fae6a5faa
sandip <sandipky@gmail.com>
2022-09-30 21:10:55 +0530

    changes to extend SpringBootServletInitializer for main application

--- src/main/java/com/techprimers/aws/AwsElasticBeanstalkExample2Application.java
+++ src/main/java/com/techprimers/aws/AwsElasticBeanstalkExample2Application.java
@@ -2,12 +2,18 @@ package com.techprimers.aws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AwsElasticBeanstalkExample2Application {
+public class AwsElasticBeanstalkExample2Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(AwsElasticBeanstalkExample2Application.class, args);
 	}
 
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(AwsElasticBeanstalkExample2Application.class);
+	}
 }
